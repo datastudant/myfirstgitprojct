db777fed118cd92a3940b9e013707d2d3448624a
w3schools-test <bengamrarida5@gmail.com>
2024-11-09 19:37:03 +0100

    parte of the Python Classes and Objects

---
+++ todo_app.py
@@ -0,0 +1,108 @@
+"""thisdict = {
+
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(len(thisdict["colors"]))
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)"""
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+"""x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change """
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+"""x = car.values()
+
+
+print(x) #before the change
+
+car["year"] = "red"
+
+print(x) #after the change"""
+"""car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change"""
+"""thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")"""
+"""thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)"""
+"""thisdict = {
+  "firstname" : "Rida",
+  "lastname" : "Bengamra",
+  "age" : 19,
+  "gmail" : "bengamraridatest@gmail.com",
+  "phone" : "+21265206***"
+}
+for x in thisdict:
+  print(x,":",thisdict[x])"""
+"""a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+"""
+
+
+"""print("hello world")"""
+"""class MyClass:
+  x = "fuuck you"
+  y = "3444"
+p1 = MyClass()
+print(p1.y)
+
+
+ttt = MyClass()
+print(ttt.x)"""
+
+"""class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        return f"السلام، سميتي {self.name} وعندي {self.age} عام."
+
+person1 = Person("أحمد", 25)
+print(person1.say_hello())  # يطبع "السلام، سميتي أحمد وعندي 25 عام.""""
+
+
+
+
+
